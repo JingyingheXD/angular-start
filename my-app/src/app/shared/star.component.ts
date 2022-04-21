1b1e7c3fc44351adae816195f511973e382aee4e
Jingying He <450883724@qq.com>
2022-04-21 14:18:13 +1000

    raising data with @output

--- my-app/src/app/shared/star.component.ts
+++ my-app/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from "@angular/core";
+import { Component, OnChanges, Input, Output, EventEmitter} from "@angular/core";
 
 @Component({
   selector: 'pm-star',
@@ -8,8 +8,14 @@ import { Component, OnChanges, Input } from "@angular/core";
 export class StarComponent implements OnChanges {
   @Input() rating: number = 4;
   cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> = 
+    new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.cropWidth = this.rating * 75/5;
   }
-}
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`)
+  }
+} 
